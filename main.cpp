cbd4d340f52b572bf4dbbc67fa0bfa6be6580158
divesh1979 <123365373+divesh1979@users.noreply.github.com>
2024-06-27 18:15:03 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,33 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
-// whether the element to be put is to safe or not
-    bool isSafe(int row,int col,vector<vector<char>>& board,char val){
-        for(int i=0;i<board.size();i++){
-            if(board[i][col]==val || board[row][i]==val){
+    // whether the element to be put is to safe or not
+    bool isSafe(int row, int col, vector<vector<char>>& board, char val) {
+        for (int i = 0; i < board.size(); i++) {
+            if (board[i][col] == val || board[row][i] == val) {
                 return false;
             }
-            if(board[3*(row/3) + i/3][3*(col/3) + i%3]==val){
+            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == val) {
                 return false;
             }
         }
         return true;
     }
 
-    bool solve(vector<vector<char>>& board){
-        int n=board.size();
-        for(int row=0;row<n;row++){
-            for(int col=0;col<n;col++){
-                if(board[row][col]=='.'){
-                    for(int val='1';val<='9';val++){
-                        if(isSafe(row,col,board,val)){
-                            board[row][col]=val;
+    bool solve(vector<vector<char>>& board) {
+        int n = board.size();
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+                if (board[row][col] == '.') {
+                    for (char val = '1'; val <= '9'; val++) {
+                        if (isSafe(row, col, board, val)) {
+                            board[row][col] = val;
                             // RECURSIVE CALL
-                            bool furtherPossible=solve(board);
-                            if(furtherPossible){
+                            bool furtherPossible = solve(board);
+                            if (furtherPossible) {
                                 return true;
-                            }else{
+                            } else {
                                 // BACKTRACKING
-                                board[row][col]='.';
+                                board[row][col] = '.';
                             }
                         }
                     }
@@ -42,3 +45,27 @@ public:
         solve(board);
     }
 };
+
+int main() {
+    vector<vector<char>> board = {
+        {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+        {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+        {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+        {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+        {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+        {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+        {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+        {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+        {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+    };
+
+    Solution().solveSudoku(board);
+    for (int i = 0; i < board.size(); i++) {
+        for (int j = 0; j < board[i].size(); j++) {
+            cout << board[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
